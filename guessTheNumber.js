819a6d284d5f8aa0a6d28159630a0423a088c969
DoniaBatool <donia1510aptech@gmail.com>
2024-03-21 23:35:18 -0700

    first commit

---
+++ guessTheNumber.js
@@ -0,0 +1,17 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let autoNum = Math.floor(Math.random() * 10);
+let user = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userNum",
+        message: "Please Enter a number between 1 to 10 : "
+    }
+]);
+let { userNum } = user;
+if (userNum === autoNum) {
+    console.log("System generated Nummber was: " + autoNum + "\n You have guess the correct number!");
+}
+else {
+    console.log("System generated Nummber was: " + autoNum + "\n Wrong Guess, Try Again! ");
+}
